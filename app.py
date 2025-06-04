1e58d4400a32d5d3b62fe6f804d3cf9552d70dc1
duriantaco <aaronoh2015@gmail.com>
2025-06-04 21:01:34 +0800

    test

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-import os
-import requests
+# import os
+# import requests
 
-class APIClient:
-    def __init__(self):
-        # This should be in environment variables!
-        self.github_token = "ghp_1234567890abcdefghijklmnopqrstuvwx123456"
-        self.jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+# class APIClient:
+#     def __init__(self):
+#         # This should be in environment variables!
+#         self.github_token = "ghp_1234567890abcdefghijklmnopqrstuvwx123456"
+#         self.jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
         
-    def call_api(self):
-        headers = {
-            "Authorization": f"Bearer {self.github_token}",
-            "X-JWT-Token": self.jwt_token
-        }
-        return requests.get("https://api.github.com/user", headers=headers)
+#     def call_api(self):
+#         headers = {
+#             "Authorization": f"Bearer {self.github_token}",
+#             "X-JWT-Token": self.jwt_token
+#         }
+#         return requests.get("https://api.github.com/user", headers=headers)
 
-if __name__ == "__main__":
-    client = APIClient()
-    print("App started successfully")
+# if __name__ == "__main__":
+#     client = APIClient()
+#     print("App started successfully")
